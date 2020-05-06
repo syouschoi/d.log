02e3955184e14efc96a45abec99664b9fbcf3140
recordar <sujin921.kim@gmail.com>
2020-05-06 12:36:58 +0900

    Apply react-router (#33)
    - Create pages folder component
    - Add Router component into pages folder
    - Apply BEM style to css class

---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Home.css';
+
+const Home = () => (
+  <div className="home">
+    <header className="home__header">
+      <h1>d.log</h1>
+    </header>
+  </div>
+);
+
+export default Home;
--- client/src/pages/Home/Home.test.js
+++ client/src/pages/Home/Home.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+import Home from './Home';
+
+test('renders d.log text', () => {
+  const { getByText } = render(<Home />);
   const logo = getByText(/d.log/i);
+
   expect(logo).toBeInTheDocument();
 });
