c19a494221bab3d5d993bb6dd1faeeebe3d71711
syouschoi <java.lang.integer.parseint@gmail.com>
2020-08-17 19:22:40 +0900

    Add input component

--- client/src/components/Input/Input.tsx
+++ client/src/components/Input/Input.tsx
@@ -1,18 +1,8 @@
 import React, { FC, forwardRef, useEffect, useRef, useState } from 'react';
-import {Props} from './Input.types';
+import { Props } from './Input.types';
 import { Input as AntdInput } from 'antd';
 
-// Type 'ForwardRefExoticComponent<Pick<Props, "onChange" | "size" | "prefix" | "type" | "accept" | "alt" | "autoComplete" | "autoFocus" | "capture" | "checked" | "crossOrigin" | "disabled" | "form" | ... 277 more ... | "allowClear"> & RefAttributes<...>>' is not assignable to type 'FC<Props>'.
-//   Types of property 'propTypes' are incompatible.
-//     Type 'WeakValidationMap<Pick<Props, "onChange" | "size" | "prefix" | "type" | "accept" | "alt" | "autoComplete" | "autoFocus" | "capture" | "checked" | "crossOrigin" | "disabled" | "form" | ... 277 more ... | "allowClear"> & RefAttributes<...>> | undefined' is not assignable to type 'WeakValidationMap<Props> | undefined'.
-//       Type 'WeakValidationMap<Pick<Props, "onChange" | "size" | "prefix" | "type" | "accept" | "alt" | "autoComplete" | "autoFocus" | "capture" | "checked" | "crossOrigin" | "disabled" | "form" | ... 277 more ... | "allowClear"> & RefAttributes<...>>' is not assignable to type 'WeakValidationMap<Props>'.
-//         Types of property 'ref' are incompatible.
-//           Type 'Validator<RefObject<HTMLInputElement> | ((instance: HTMLInputElement | null) => void) | null | undefined> | undefined' is not assignable to type 'Validator<RefObject<HTMLInputElement>> | undefined'.
-//             Type 'Validator<RefObject<HTMLInputElement> | ((instance: HTMLInputElement | null) => void) | null | undefined>' is not assignable to type 'Validator<RefObject<HTMLInputElement>>'.
-//               Type 'RefObject<HTMLInputElement> | ((instance: HTMLInputElement | null) => void) | null | undefined' is not assignable to type 'RefObject<HTMLInputElement>'.
-//                 Type 'undefined' is not assignable to type 'RefObject<HTMLInputElement>'.ts(2322)
-
-// const Input: FC<Props> =  forwardRef<HTMLInputElement,Props>( ( {ref, autoFocus = false , onChange, ...rest }) => {
+// const Input = forwardRef<HTMLInputElement, Props>(({ autoFocus = false , onChange, …rest }, ref) => {
 //     const localRef = useRef(null);
 //     const [value, setValue] = useState('');
 
@@ -25,28 +15,7 @@ import { Input as AntdInput } from 'antd';
 //         onChange && onChange(e.target.value);
 //     };
 
-//     return <AntdInput ref={autoFocus ? localRef : ref} onChange={handleChange} {...rest} />;
+//     return <AntdInput {...rest} ref={autoFocus ? localRef : ref} onChange={handleChange} />;
 // });
 
-export default Input;
-
-// const Input = forwardRef(({ref, autoFocus = false , onChange, ...rest }) => {
-//     const localRef = useRef(null);
-//     const [value, setValue] = useState('');
-
-//     useEffect(() => {
-//         localRef.current && localRef.current.focus();
-//     }, []);
-
-//     const handleChange = e => {
-//         setValue(e.target.value);
-//         onChange && onChange(e.target.value);
-//     };
-
-//     return <AntdInput ref={autoFocus ? localRef : ref} onChange={handleChange} {...rest} />;
-// });
 // export default Input;
-
-// onChange = (e: React.ChangeEvent<HTMLInputElement>)=> {
-//     const newValue = e.target.value;
-//  }
